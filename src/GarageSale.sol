932e75cecfc88fd91913c3e7b0ede4d6ef0e5384
Devin A. Conley <devinaconley@gmail.com>
2023-12-05 00:18:47 -0500

    harden configuration

--- src/GarageSale.sol
+++ src/GarageSale.sol
@@ -1,36 +1,102 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.23;
 
 contract GarageSale {
     // events
-    event Sold(address seller, address token, uint256 id);
-    event Purchased(address buyer, address[] tokens, uint56[] ids);
+    event Sell(address indexed seller, address indexed token, uint256 id);
+    event Buy(address indexed buyer, address[] tokens, uint56[] ids);
+    event OfferUpdated(uint256 offer);
+    event AuctionUpdated(uint256 min, uint256 max, uint256 duration);
+    event TakeUpdated(uint256 take);
+    event TokenUpdated(address indexed token, bool enabled);
+    event ControllerUpdated(address controller);
 
-    // fields
-    uint256 public offer;
-    uint256 public min;
-    uint256 public max;
-    uint256 public duration;
-    mapping(address => bool) public collections;
+    // types
+    struct Token {
+        bool enabled;
+        uint64 count;
+    }
 
-    // admin configuration
-    function setOffer(uint256 offer_) public {
+    // config
+    uint256 public offer; // wei
+    uint256 public min; // wei
+    uint256 public max; // wei
+    uint32 public duration; // seconds
+    uint8 public bundle; // size
+    uint16 public take; // fee (out of 1e3)
+    address public controller;
+
+    // data
+    mapping(address => Token) public tokens;
+    uint256 fees;
+
+    /**
+     * @notice initialize garage sale contract with reasonable defaults
+     */
+    constructor() {
+        offer = 1e14; // 0.0001 ether
+        min = 1e15; // 0.001 ether
+        max = 1e17; // 0.1 ether
+        duration = 15 * 60; // 15 minutes
+        bundle = 4;
+        take = 1e2; // 10%
+        controller = msg.sender;
+    }
+
+    modifier onlyController() {
+        require(msg.sender == controller, "sender is not controller");
+        _;
+    }
+
+    // --- configuration ------------------------------------------------------
+
+    /**
+     * @param offer_ new offer price (in wei) to sell a token to the protocol
+     */
+    function setOffer(uint256 offer_) public onlyController {
+        require(offer_ > 0, "offer is zero");
+        require(offer_ < min / bundle, "offer too high");
         offer = offer_;
+        emit OfferUpdated(offer_);
     }
 
-    function setMin(uint256 min_) public {
+    /**
+     * @notice update params for dutch auction resale
+     * @param min_ new minimum price
+     * @param max_ new maximum price
+     * @param duration_ new auction duration
+     */
+    function setAuction(
+        uint256 min_,
+        uint256 max_,
+        uint256 duration_
+    ) public onlyController {
+        require(min_ > bundle * offer, "min too low");
+        require(min_ <= max_, "min is greater than max price");
         min = min_;
-    }
-
-    function setMax(uint256 max_) public {
         max = max_;
+        duration = uint32(duration_);
+        emit AuctionUpdated(min_, max_, duration_);
     }
 
-    function setDuration(uint256 duration_) public {
-        duration = duration_;
+    /**
+     * @param take_  new take rate fee
+     */
+    function setTake(uint256 take_) public onlyController {
+        require(take < 1e3, "fee too high");
+        take = uint16(take_);
+        emit TakeUpdated(take_);
     }
 
-    function setCollection(address collection, bool enabled) public {
-        collections[collection] = enabled;
+    function setToken(address token, bool enabled) public onlyController {
+        // TODO verify token type
+        tokens[token].enabled = enabled;
+        emit TokenUpdated(token, enabled);
+    }
+
+    function setController(address controller_) public {
+        // TODO only owner
+        controller = controller_;
+        emit ControllerUpdated(controller_);
     }
 }
--- test/Configuration.t.sol
+++ test/Configuration.t.sol
@@ -1,39 +1,97 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {GarageSale} from "../src/GarageSale.sol";
 
 contract ConfigurationTest is Test {
     GarageSale gs;
+    address alice;
+    address bob;
 
     function setUp() public {
         gs = new GarageSale();
+        alice = address(0xa11ce);
+        bob = address(0x808);
     }
 
     function test_OfferPrice() public {
+        vm.expectEmit(address(gs));
+        emit GarageSale.OfferUpdated(1e14);
         gs.setOffer(1e14); // 0.0001
         assertEq(gs.offer(), 1e14);
     }
 
-    function test_MinAuctionPrice() public {
-        gs.setOffer(1e15); // 0.001
-        assertEq(gs.offer(), 1e15);
+    function test_OfferPriceZero() public {
+        vm.expectRevert("offer is zero");
+        gs.setOffer(0);
     }
 
-    function test_MaxAuctionPrice() public {
-        gs.setOffer(1e17); // 0.1
-        assertEq(gs.offer(), 1e17);
+    function test_OfferPriceHigh() public {
+        vm.expectRevert("offer too high");
+        gs.setOffer(5e14);
     }
 
-    function testFuzz_SetDuration(uint256 x) public {
-        gs.setDuration(x);
-        assertEq(gs.duration(), x);
+    function test_OfferPriceUnauthorized() public {
+        vm.expectRevert("sender is not controller");
+        vm.prank(alice);
+        gs.setOffer(1e13);
     }
 
-    function test_CollectionWhitelist() public {
+    function testFuzz_OfferPrice(uint256 x) public {
+        vm.assume(x > 0);
+        vm.assume(x < 1e15 / 4);
+        gs.setOffer(x);
+        assertEq(gs.offer(), x);
+    }
+
+    function test_AuctionConfig() public {
+        vm.expectEmit(address(gs));
+        emit GarageSale.AuctionUpdated(5e14, 2e17, 1800);
+        gs.setAuction(5e14, 2e17, 1800); // 0.2 -> 0.0005 over 30 min
+        assertEq(gs.min(), 5e14);
+        assertEq(gs.max(), 2e17);
+        assertEq(gs.duration(), 1800);
+    }
+
+    function test_AuctionUnauthorized() public {
+        vm.expectRevert("sender is not controller");
+        vm.prank(alice);
+        gs.setAuction(5e14, 2e17, 1800);
+    }
+
+    function test_TakeRate() public {
+        vm.expectEmit(address(gs));
+        emit GarageSale.TakeUpdated(30);
+        gs.setTake(30); // 3%
+        assertEq(gs.take(), 3e1);
+    }
+
+    function test_TakeUnauthorized() public {
+        vm.expectRevert("sender is not controller");
+        vm.prank(alice);
+        gs.setTake(8);
+    }
+
+    function test_TokenWhitelist() public {
         address tkn = address(12345);
-        gs.setCollection(tkn, true);
-        assertEq(gs.collections(tkn), true);
+        vm.expectEmit(address(gs));
+        emit GarageSale.TokenUpdated(tkn, true);
+        gs.setToken(tkn, true);
+        (bool enabled, ) = gs.tokens(tkn);
+        assertEq(enabled, true);
+    }
+
+    function test_TokenUnauthorized() public {
+        vm.expectRevert("sender is not controller");
+        vm.prank(alice);
+        gs.setToken(address(333), false);
+    }
+
+    function test_Controller() public {
+        vm.expectEmit(address(gs));
+        emit GarageSale.ControllerUpdated(bob);
+        gs.setController(bob);
+        assertEq(gs.controller(), bob);
     }
 }
