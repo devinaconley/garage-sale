bfa11d8bbddc8620ba5a24553987844beaeac420
Devin A. Conley <devinaconley@gmail.com>
2023-12-30 18:50:05 -0600

    update create2 salt

--- script/GarageSale.s.sol
+++ script/GarageSale.s.sol
@@ -11,7 +11,7 @@ contract GarageSaleDeploy is Script {
         address owner = 0x55f225153488E9f75213d0BF4b44920f76b7C69c;
         // bytes32 h = keccak256(abi.encodePacked(type(GarageSale).creationCode, abi.encode(owner)));
         // console2.logBytes32(h);
-        bytes32 salt = 0xd7945335d940ea56b1993bc273db27460dce39e5c6f743831ea76041cfbe6c3b; // mined for leading zeros
+        bytes32 salt = 0x1a50fd5c959a90f8a8764cb1d9e8665d3d68607d56996b41b49b70154206605e; // mined for leading zeros
 
         vm.startBroadcast();
         GarageSale gs = new GarageSale{salt: salt}(owner);
